13b8da60c2311040d7cab38e1303a9be8426e5fc
diabolical2314 <bueomer@gmail.com>
2024-12-01 00:02:35 +0800

    Final

--- src/script.js
+++ src/script.js
@@ -19,9 +19,15 @@ const scene = new THREE.Scene()
 /**
  * Textures
  */
+
 const textureLoader
  = new THREE.TextureLoader()
 const matcapTexture = textureLoader.load('textures/matcaps/8.png')
+
+
+
+
+
 
 /**
  * Fonts
